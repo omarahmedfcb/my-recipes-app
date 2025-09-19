729483fd3a1a89c561398cb28b0e07f9da5a02aa
unknown <manoomarfcb2000@gmail.com>
2025-09-19 19:59:49 +0300

    final commit for github

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import RecipeDetails from "./Pages/RecipeDetails";
 import Products from "./Pages/Products";
@@ -11,7 +11,7 @@ import Register from "./Pages/Register";
 import { Toaster } from "react-hot-toast";
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Toaster position="top-center" reverseOrder={false} />
       <Routes>
         <Route path="/sign" element={<LogRegPage />}>
@@ -27,7 +27,7 @@ function App() {
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
